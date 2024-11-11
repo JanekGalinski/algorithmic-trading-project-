a9dcc21d255aa4be4cf5bc3028163352a67d78bd
Jan Galiński <jangalinskiphoto@gmail.com>
2024-11-11 13:35:13 +0100

    Add descriptions

--- project_code.py
+++ project_code.py
@@ -15,25 +15,30 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 # %%
 #-----------------------------------------------------------------------------------------------------------------------------
 # 2) Downloading data
 #-----------------------------------------------------------------------------------------------------------------------------
 
+#BND stands for Vanguard Total Bond Market ETF (bonds ETF)
 BND = pd.read_csv("BND ETF Stock Price History.csv", parse_dates=['Date'], index_col='Date')
+#GBP_USD stands for British Pound Sterling / US Dollar (FX)
 GBP_USD = pd.read_csv("GBP_USD Historical Data.csv", parse_dates=['Date'], index_col='Date')
+#MSFT stands for Microsoft Corporation (stock)
 MSFT = pd.read_csv("MSFT Historical Data.csv", parse_dates=['Date'], index_col='Date')
+#ND100 stands for NASDAQ 100 Index (index)
 ND100 = pd.read_csv("Nasdaq 100 Historical Data.csv", parse_dates=['Date'], index_col='Date')
+#NG stands for Natural Gas Futures (future)
 NG = pd.read_csv("Natural Gas Futures Historical Data.csv", parse_dates=['Date'], index_col='Date')
 
 
 # %%
 #-----------------------------------------------------------------------------------------------------------------------------
-# 3) Visualizing price across time
+# 3) Visualizing price of each asset across time
 #-----------------------------------------------------------------------------------------------------------------------------
 
+#plot BND
 plt.figure(figsize=(12, 6))
 plt.plot(BND.index, BND['Price'], label='Price', color='blue')
 plt.title('BND Price Over Time')
@@ -43,6 +48,7 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
+#plot GBP_USD
 plt.figure(figsize=(12, 6))
 plt.plot(GBP_USD.index, GBP_USD['Price'], label='Price', color='green')
 plt.title('GBP_USD Price Over Time')
@@ -52,6 +58,7 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
+#plot MSFT
 plt.figure(figsize=(12, 6))
 plt.plot(MSFT.index, MSFT['Price'], label='Price', color='pink')
 plt.title('MSFT Price Over Time')
@@ -61,6 +68,7 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
+#plot ND100
 plt.figure(figsize=(12, 6))
 plt.plot(ND100.index, ND100['Price'], label='Price', color='blue')
 plt.title('ND100 Price Over Time')
@@ -70,6 +78,7 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
+#plot NG
 plt.figure(figsize=(12, 6))
 plt.plot(NG.index, NG['Price'], label='Price', color='green')
 plt.title('NG Price Over Time')
@@ -82,13 +91,18 @@ plt.show()
 
 # %%
 #-----------------------------------------------------------------------------------------------------------------------------
-# 4) Basic statistics of price across time
+# 4) Basic statistics of price of each asset across time
 #-----------------------------------------------------------------------------------------------------------------------------
 
+#BND statistics
 print(BND['Price'].describe())
+#GBP_USD statistics
 print(GBP_USD['Price'].describe())
+#MSFT statistics
 print(MSFT['Price'].describe())
+#ND100 statistics
 print(ND100['Price'].describe())
+#NG statistics
 print(NG['Price'].describe())
 
 # %%
@@ -96,7 +110,24 @@ print(NG['Price'].describe())
 # 5) Defining strategy
 #-----------------------------------------------------------------------------------------------------------------------------
 
+#Strategy
+#The selected strategy for signal is trend-following using moving average crossover
+#It is quite simple and popular strategy among investors 
+#The objective is to identify the direction of market trend and use it to advantage
+#There will be defined two moving averages (MA): one short term and one long term
+#Buy signal will be defined as when short term MA crosses above long term MA
+#Sell signal will be defined as when short term MA crosses below long term MA
+#After buy signal position will be held until a sell signal
+#Similarly after sell signal position will be held until a buy signal
 
+#Economic reasoning
+#The key assumption of this strategy arguing that it should work is basing on price momentum
+#As a result trends tend to persist for given time period and allow to take profit by leveraging them
+#What is more taking into account long term MA, allows to filter out only short term noise/fluctuations
+#On the other hand this strategy can fail due to only basing on historical data (lagging) and failing to quickly respond
+#Lastly this strategy can have limitations in markets where there are no clear trends present
+
+#define strategy function
 def trend_following_strategy(data, price_column, short_window=20, long_window=50):
     
     #data load
@@ -139,7 +170,11 @@ def trend_following_strategy(data, price_column, short_window=20, long_window=50
     
     return df
 
-result_df = trend_following_strategy(data=NG, price_column='Price')
-
+#results per each asset
+results_BND = trend_following_strategy(data=BND, price_column='Price')
+results_GBP_USD = trend_following_strategy(data=GBP_USD, price_column='Price')
+results_MSFT = trend_following_strategy(data=MSFT, price_column='Price')
+results_ND100 = trend_following_strategy(data=ND100, price_column='Price')
+results_NG = trend_following_strategy(data=NG, price_column='Price')
 
 # %%
